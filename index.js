f95c762fbe290dc2a3c68462fb191727810956c1
manish pandey <manishpandey.rs.che23@itbhu.ac.in>
2024-02-25 22:46:32 +0530

    prime check

---
+++ index.js
@@ -0,0 +1,16 @@
+function check_prime(num){
+    let count =0;
+    for(let i=1;i<num;i++){
+        if(num %i !=0){
+            count ++;
+        }
+    }
+      if(count==2){
+
+      }else{
+    return flag
+}
+}
+check_prime(console.log(9));
+check_prime(console.log(8));
+
